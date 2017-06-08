d7803cd39d4acbbe82fbbf3dc5dad961ab89e50e
saranyanarayanasamy <saranyasaran997@gmail.com>
2017-06-08 18:46:39 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class large
+{
+public static void main(String arg[])
+{
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b[]=new Int[a];
+for(int k=0;k<a;k++)
+{
+a[k]=sacn.nextInt();
+}
+int max=b[0];
+for(int i=0;i<a;i++
+{
+System.out.println("the number is "+a[i]);
+}
+}
+}
+}
